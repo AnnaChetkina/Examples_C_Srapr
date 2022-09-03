424d1ebf9752aadc431cfb2cfc844e7243764e46
Anna <a.chetkina.a@yandex.ru>
2022-09-03 23:58:16 +0300

    add random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,6 +1,8 @@
 ﻿// int = integer - целые числа
 // в наименовании переменной цифра не д стоять на первом месте, также не принято использовать _ для именования переменных 
-int numberA = 9;
-int numberB = 5;
+int numberA = new Random().Next(1, 10); // 1 2 ... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int sum = numberA + numberB;
 Console.WriteLine(sum);
