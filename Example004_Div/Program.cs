db8f3b9ad6bd346ac1e6b7db1c3b62e9cf536adb
Anna <a.chetkina.a@yandex.ru>
2022-09-03 23:42:43 +0300

    add Example 004 Div v.2

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿int numberA = 12;
+int numberB = 5;
+Console.WriteLine(numberA / numberB); // получаем 2, потому что работаем с целыми числами
+
+// тип данных double позволяет работать с вещественными числами
+double numberC = 12;
+double numberD = 5;
+Console.WriteLine(numberC / numberD);
