c7a96f3a7ad278f4fab24d7c02a15bc0ec349db2
Anna <a.chetkina.a@yandex.ru>
2022-09-03 23:31:21 +0300

    add Example 003 Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿// int = integer - целые числа
+// в наименовании переменной цифра не д стоять на первом месте, также не принято использовать _ для именования переменных 
+int numberA = 9;
+int numberB = 5;
+int sum = numberA + numberB;
+Console.WriteLine(sum);
