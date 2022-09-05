a6ba25fe6be1ea79c1cfdff7bed7c1b36142708e
Anna <a.chetkina.a@yandex.ru>
2022-09-05 23:54:09 +0300

    add example 010

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,12 @@
+﻿int [] array = {11, 31, 31, 41, 15, 15, 61, 17, 18, 19};
+int n = array.Length;
+int find = 15;
+int ind = 0;
+
+while (ind < n) {
+    if (array[ind] == find){
+        Console.WriteLine(ind);
+        break;
+    }
+    ind++;
+}
